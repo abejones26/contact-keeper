5da81e4ae326857097c2f0950c51db1703efeef3
Abraham Jones <56662623+abejones26@users.noreply.github.com>
2021-03-27 23:10:20 -0500

    Cleanup, Navbar & Router Setup

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { FaRegAddressBook } from 'react-icons/fa';
+
+const Navbar = ({ title, icon }) => {
+  return (
+    <div className='navbar bg-primary'>
+      <h1>
+        <FaRegAddressBook /> {title}
+      </h1>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+};
+
+Navbar.defaultProps = {
+  title: 'Contact keeper',
+  icon: '',
+};
+
+export default Navbar;
