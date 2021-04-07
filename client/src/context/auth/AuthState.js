8dcac9c231f5401ea5bddda5eb0921c2b5f4f52b
Abraham Jones <56662623+abejones26@users.noreply.github.com>
2021-04-06 20:48:27 -0500

    User Login

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -71,7 +71,29 @@ const AuthState = (props) => {
   };
 
   // Login User
+  const login = async (formData) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
 
+    try {
+      const res = await axios.post('/api/auth', formData, config);
+
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data,
+      });
+
+      loadUser();
+    } catch (err) {
+      dispatch({
+        type: LOGIN_FAIL,
+        payload: err.response.data.msg,
+      });
+    }
+  };
   // Logout
 
   // Clear Errors
@@ -88,6 +110,7 @@ const AuthState = (props) => {
         register,
         clearErrors,
         loadUser,
+        login,
       }}
     >
       {props.children}
