f141b75a99db9071e2b278a987595cad0760be80
Abraham Jones <56662623+abejones26@users.noreply.github.com>
2021-04-07 19:54:47 -0500

    Add Contact to database from UI form

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -6,8 +6,10 @@ import {
   UPDATE_CONTACT,
   FILTER_CONTACTS,
   CLEAR_FILTER,
+  CONTACT_ERROR,
 } from '../types';
 
+// eslint-disable-next-line
 export default (state, action) => {
   switch (action.type) {
     case ADD_CONTACT:
@@ -51,6 +53,11 @@ export default (state, action) => {
       return {
         ...state,
         filtered: null,
+      };
+    case CONTACT_ERROR:
+      return {
+        ...state,
+        error: action.payload,
       };
     default:
       return state;
